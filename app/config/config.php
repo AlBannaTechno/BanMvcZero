cc07fbef73974b10a0c34766cd72aca33689bcb4
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2020-01-07 19:46:09 +0200

    [Core] Create config.php to contains all custom global constants

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+// App Root
+define('APP_ROOT', dirname(__FILE__, 2));
+
+// URL ROOT
+
+define('URL_ROOT', 'http://localhost/BanMVC');
+
+// SITE NAME
+
+define('SITE_NAME', 'BanMvcImp');
