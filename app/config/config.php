9603ac831d77724b6267a86902aae0c6b9af975e
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2020-01-07 22:34:33 +0200

    [Core][Model][Config] define __GLOB__MODEL__ at config.php

--- app/config/config.php
+++ app/config/config.php
@@ -20,3 +20,4 @@ define('__LAYOUT__', '_layout.php');
 define('__GLOB__BODY__', '__BODY__');
 define('__GLOB__CONTROLLER_TITLE__', '__CONTROLLER_TITLE__');
 define('__GLOB__CONTROLLER_ACTION_TITLE__', '__CONTROLLER_ACTION_TITLE__');
+define('__GLOB__MODEL__', '__MODEL__');
