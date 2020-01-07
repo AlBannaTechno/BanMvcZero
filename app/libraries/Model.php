b190419cf4eac910a19cd3b990865793ca0fedfc
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2020-01-07 22:33:13 +0200

    [Core][Model] Create Base Model.php class to support general models functions Also implement load() to return current model of view

---
+++ app/libraries/Model.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Class Model
+ *
+ * Any Model class should extends this class to support Intellisense
+ * Then Call ModeClassName::load() , and assign it to any variable
+ * Also notice : $model , still available in any view , but we can override it
+ * And also notice , $model still override but without Intellisense
+ *
+ * This is the only way i figure it to force PHP to work correctly in this generic scenario
+ * [PHP does not support generics]
+ */
+class Model {
+    public static function load() : self {
+        return Heleprs\Model\loadModel();
+    }
+}
