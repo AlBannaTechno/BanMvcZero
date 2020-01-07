3ed58d1a91d7bf11578535e0ffa866dc7e2b95de
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2020-01-07 19:46:50 +0200

    [Core] load config in bootstrap.php , also auto load all core libs

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,17 @@
 <?php
+
+// Loading Config
+require_once 'config/config.php';
+
+echo APP_ROOT;
 // Loading Libraries
-require_once 'libraries/Core.php';
-require_once 'libraries/Controller.php';
-require_once 'libraries/Database.php';
+//require_once 'libraries/Core.php';
+//require_once 'libraries/ControllerBase.php';
+//require_once 'libraries/Database.php';
+
+
+// Auto Load Core Libraries
+// All ClassNames need to match File name
+spl_autoload_register(static function($className){
+    require_once 'libraries/' . $className . '.php';
+});
