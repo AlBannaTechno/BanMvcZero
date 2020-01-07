5f8e54a6ecee79d6aecf526c9a0ee5be88180030
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2020-01-07 19:48:11 +0200

    [Core] Test passing data from controller to view

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -5,17 +5,13 @@ class Home extends ControllerBase
 {
     public function __construct()
     {
-        echo '[Home]{INIT}';
-        echo '<br/>';
     }
 
     public function index(){
-        echo '[[Index] Of [Home]]';
-        $this->view('index');
+        $this->view(['title' => 'Welcome From Index Of Home']);
     }
 
     public function about($id, $name){
-        echo '[[about] Of [Home]] ' . $id . ' ' . $name;
     }
 
 }
