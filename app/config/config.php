958e259285b22c779e947457bde4c469a07c5b18
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2020-01-07 21:27:31 +0200

    [Core][Configuration] define __GLOB constants to referer to global references

--- app/config/config.php
+++ app/config/config.php
@@ -14,3 +14,9 @@ define('SITE_NAME', 'BanMvcImp');
 // define The Main Layout Name , from ../app/views/
 
 define('__LAYOUT__', '_layout.php');
+
+// declare specific globals => globals return string to other globals
+
+define('__GLOB__BODY__', '__BODY__');
+define('__GLOB__CONTROLLER_TITLE__', '__CONTROLLER_TITLE__');
+define('__GLOB__CONTROLLER_ACTION_TITLE__', '__CONTROLLER_ACTION_TITLE__');
