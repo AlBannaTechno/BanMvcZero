d47dd928b6cc521d5a1d53f9b0b2b2e466b708a2
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2020-01-08 09:47:38 +0200

    [Core][Config] define __CORE_FEATURES_SUPPORT_AREA__

--- app/config/config.php
+++ app/config/config.php
@@ -21,3 +21,7 @@ define('__GLOB__BODY__', '__BODY__');
 define('__GLOB__CONTROLLER_TITLE__', '__CONTROLLER_TITLE__');
 define('__GLOB__CONTROLLER_ACTION_TITLE__', '__CONTROLLER_ACTION_TITLE__');
 define('__GLOB__MODEL__', '__MODEL__');
+
+// Config CORE FEATURES
+
+define('__CORE_FEATURES_SUPPORT_AREA__', TRUE);
