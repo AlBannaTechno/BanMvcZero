e32b99c3abcf20ffe6741694dd4ba909fd056035
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2020-01-08 09:49:06 +0200

    [Core][Bootstrap] load core libraries based on area feature

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -8,12 +8,28 @@ require_once 'config/config.php';
 //require_once 'libraries/ControllerBase.php';
 //require_once 'libraries/Database.php';
 
+//require_once 'librariesArea/AreaCore.php';
+//require_once 'librariesArea/AreaControllerBase.php';
+//require_once 'librariesArea/AreaModel.php';
+
+
 
 // Auto Load Core Libraries
 // All ClassNames need to match File name
-spl_autoload_register(static function($className){
-    require_once 'libraries/' . $className . '.php';
-});
+
+
+if (__CORE_FEATURES_SUPPORT_AREA__){
+    spl_autoload_register(static function($className){
+        $dbt=debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS,2);
+        $caller = $dbt[1]['function'];
+        require_once 'librariesArea/' . $className . '.php';
+    });
+} else {
+    spl_autoload_register(static function($className){
+        require_once 'libraries/' . $className . '.php';
+    });
+}
+
 
 // include helpers
 // here we must not include any optional helpers , only VI helpers
