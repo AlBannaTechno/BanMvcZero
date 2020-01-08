0485ea36a9fa20dbe128e92414858732d6ca6768
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2020-01-08 10:48:55 +0200

    [Core][Area][Defaults][fIX] Typo in config.php

---
+++ app/Areas/Customers/_defaults.php
@@ -0,0 +1,6 @@
+<?php
+// next line will allow to request
+//      http://localhost/BanMVC/Customers/
+// As   http://localhost/BanMVC/Customers/Payment
+// As   http://localhost/BanMVC/Customers/Payment/Index
+$GLOBALS[__GLOB__AREA__DEFAULT_CONTROLLER] = 'Payment';
