1410b152cb684819118c66233444009954071258
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2020-01-09 11:45:47 +0200

    [Core][IoC] Register Core Types Into IoC Container

--- public/index.php
+++ public/index.php
@@ -3,5 +3,13 @@ require_once '../core/bootstrap.php';
 //$core = new  AreaCore();
 $container = new Container();
 $container->provide(CoresProvider::class);
+$container->provide(ControllerBase::class);
+$container->provide(Core::class);
+$container->provide(Model::class);
+$container->provide(AreaControllerBase::class);
+$container->provide(AreaCore::class);
+$container->provide(AreaModel::class);
+$container->provide(PdoDatabase::class);
+
 $provider = $container->resolve(CoresProvider::class);
 //$provider = new CoresProvider();
