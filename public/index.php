2bcb3acad04657bab88a5b0dd2951f3083d9c1b7
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2020-01-09 11:56:56 +0200

    [Core][IoC][Test][OK] built and use the IoC new container system

--- public/index.php
+++ public/index.php
@@ -2,14 +2,7 @@
 require_once '../core/bootstrap.php';
 //$core = new  AreaCore();
 $container = new Container();
-$container->provide(CoresProvider::class);
-$container->provide(ControllerBase::class);
-$container->provide(Core::class);
-$container->provide(Model::class);
-$container->provide(AreaControllerBase::class);
-$container->provide(AreaCore::class);
-$container->provide(AreaModel::class);
-$container->provide(PdoDatabase::class);
-
+register_core_types($container);
+$container->build();
 $provider = $container->resolve(CoresProvider::class);
 //$provider = new CoresProvider();
